68c4542054daf8a768f8ee475b7538276aedab72
GGhiaroni <gghiaronitiziano@gmail.com>
2026-07-16 22:42:05 -0300

    bootstrap projeto

---
+++ src/main/java/com/gabriel/tiziano/teamheritage/TeamheritageApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.tiziano.teamheritage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamheritageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamheritageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/tiziano/teamheritage/TeamheritageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.tiziano.teamheritage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamheritageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
